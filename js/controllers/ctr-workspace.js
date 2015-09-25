ad5fb632242fb039e6c6c6a2179fb4c30fc22861
Ghislaine Guerin <me@ghislaineguerin.com>
2015-09-25 09:46:16 -0400

    sub company banner fix

--- js/controllers/ctr-workspace.js
+++ js/controllers/ctr-workspace.js
@@ -2,9 +2,11 @@
 
 angular.module('risevision.editorApp.controllers')
   .controller('WorkspaceController', ['$scope', 'editorFactory',
-    'placeholderFactory',
-    function ($scope, editorFactory, placeholderFactory) {
+    'placeholderFactory', 'userState',
+    function ($scope, editorFactory, placeholderFactory, userState) {
       $scope.factory = editorFactory;
       $scope.placeholderFactory = placeholderFactory;
+      $scope.isSubcompanySelected = userState.isSubcompanySelected;
+      $scope.isTestCompanySelected = userState.isTestCompanySelected;
     }
   ]); //ctr
